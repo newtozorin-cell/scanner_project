2dff502858a1f02a437fc980e9a87b58aaf205a4
newtozorin-cell <newtozorin@gmail.com>
2026-07-04 17:31:26 +0200

    Fix dependency conflict and env variables

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-CLIENT_ID    = "ABC-100"
-SECRET_KEY   = "ABC"
-REDIRECT_URI = "https://trade.fyers.in/api-login/redirect-uri/index.html"
+import os
+
+CLIENT_ID    = os.environ.get("FYERS_APP_ID", "ABC-100")
+SECRET_KEY   = os.environ.get("FYERS_SECRET_ID", "ABC")
+REDIRECT_URI = os.environ.get("FYERS_REDIRECT_URI", "https://trade.fyers.in/api-login/redirect-uri/index.html")
 STATE        = "scanner_state"
