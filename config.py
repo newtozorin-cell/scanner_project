a15c6fd6a91ffee0037f34a1b07d2ef7d2cdff6a
newtozorin-cell <newtozorin@gmail.com>
2026-07-24 11:17:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 CLIENT_ID    = os.environ.get("FYERS_APP_ID", "ABC-100")
 SECRET_KEY   = os.environ.get("FYERS_SECRET_ID", "ABC")
-REDIRECT_URI = os.environ.get("FYERS_REDIRECT_URI", "https://trade.fyers.in/api-login/redirect-uri/index.html")
+REDIRECT_URI = os.environ.get("FYERS_REDIRECT_URI", "https://scanner-project-jlft.onrender.com/auth/callback")
 STATE        = "scanner_state"
